95de4e908c05afa8425def873a7a51d7b42dc5dd
otegami <a.s.takuya1026@gmail.com>
2021-09-19 20:00:07 -0700

    Added sing in workflow

---
+++ test/graphql/mutations/sigin_in_user_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class Mutations::SignInUserTest < ActiveSupport::TestCase
+  def perform(**args)
+    Mutations::SignInUser.new(object: nil, field: nil, context: {}).resolve(**args)
+  end
+
+  def create_user
+    User.create!(
+      name: 'Test User',
+      email: 'test@example.com',
+      password: '[omitted]'
+    )
+  end
+
+  test 'sucess' do
+    user = create_user
+    result = perform(credentials: { email: user.email, password: user.password })
+
+    assert result[:token].present?
+    assert_equal result[:user], user
+  end
+
+  test 'failure beacause of no credentials' do
+    assert_nil perform
+  end
+
+  test 'failure beacause of wrong email' do
+    user = create_user
+    assert_nil perform(credentials: { email: 'wrong', password: user.password })
+  end
+
+  test 'failure because of wrong password' do
+    user = create_user
+    assert_nil perform(credentials: { email: user.password, password: 'wrong'})
+  end
+end
